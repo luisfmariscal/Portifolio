5255229a44fb9a069d8299a09d5cdab2082c717e
Luis F Mariscal <109770715+luisfmariscal@users.noreply.github.com>
2025-03-12 07:59:40 -0400

    retirando importe falask_mail

--- app.py
+++ app.py
@@ -1,22 +1,10 @@
 from flask import Flask, render_template, redirect, request, flash
-from flask_mail import Mail, Message
 from dotenv import load_dotenv
 import os
 
 app = Flask(__name__)
 app.secret_key = 'Fernando'
 
-mail_settings = {
-    "MAIL_SERVER": 'smtp.gmail.com',
-    "MAIL_PORT": 465,
-    "MAIL_USE_TLS": False,
-    "MAIL_USE_SSL": True,
-    "MAIL_USERNAME": os.getenv("EMAIL"),
-    "MAIL_PASSWORD": os.getenv("SENHA")
-}
-
-app.config.update(mail_settings)
-mail = Mail(app)
 
 class Contato:
     def __init__(self, nome, email, mensagem):
@@ -30,27 +18,7 @@ def index():
 
 @app.route('/send', methods=['GET', 'POST'])
 def send():
-    if request.method == 'POST':
-        formContato = Contato(
-            request.form["nome"],
-            request.form["email"],
-            request.form["mensagem"]
-        )
-
-        msg = Message(
-            subject = f'{formContato.nome} te enviou uma mensagem no portfólio',
-            sender = app.config.get("MAIL_USERNAME"),
-            recipients= ['luisfmariscal@gmail.com', app.config.get("MAIL_USERNAME")],
-            body = f'''
-            
-            {formContato.nome} com o e-mail {formContato.email}, te enviou a seguinte mensagem:
-
-            {formContato.mensagem}
-
-            '''
-        )
-        mail.send(msg)
-        flash('Mensagem enviada com sucesso!')
+    
     return redirect('/')
 
 if __name__ == '__main__':
